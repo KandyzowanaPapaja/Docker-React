b61dd3244441cd2bc4f44fd4e4c1aade0f6879ac
Rafał Gabrysiak <rafal.gabrysiak7@gmail.com>
2022-01-15 19:33:11 +0100

    Updated text 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Best way to learn react today <code>src/App.js</code> and save to reload.
+          Best way to get known with react today <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
